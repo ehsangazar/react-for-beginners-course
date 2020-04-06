4bb45d7981243ab24e2f281b86642ec735d1a312
Ehsan Gazar <ehsan.gazar@beteasy.com.au>
2020-04-06 11:54:30 +1000

    fix: fourth lesson - component

---
+++ App.js
@@ -0,0 +1,13 @@
+const App = (
+    <div>
+        <h1>This is title</h1>
+        <p>
+            Consectetur non nulla Lorem cillum. Adipisicing amet commodo laboris minim ad amet dolor excepteur culpa laborum magna. Nulla tempor elit officia mollit consequat ea exercitation dolore duis occaecat id excepteur esse sit.
+        </p>
+        <Button />
+    </div>
+)
+ReactDOM.render(
+    App,
+    document.getElementById('app')
+)
