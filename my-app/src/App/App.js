6f6e49b39b64a7b0a8d9c30e2168693eb19a8f11
Ehsan Gazar <ehsan.gazar@beteasy.com.au>
2020-04-07 15:09:27 +1000

    fix: sixth lesson - component css

---
+++ my-app/src/App/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './App.css';
+import Button from './../Button/Button'
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="heading1">
+        This is the title
+      </h1>
+      <p>
+        Eiusmod excepteur cillum aliqua sunt aliquip. Ea ad voluptate veniam excepteur adipisicing adipisicing nostrud dolor deserunt ipsum nisi. Do ipsum esse irure cillum veniam commodo aute nulla anim nulla ex. Ullamco enim non deserunt aliquip eu ad Lorem voluptate adipisicing nostrud aliquip proident occaecat.
+      </p>
+      <Button />
+    </div>
+  );
+}
+
+export default App;
---
+++ my-app/src/Button/Button.js
@@ -0,0 +1,6 @@
+import React from 'react'
+import './Button.css'
+
+const Button = () => <button className="button">Click Here</button>;
+
+export default Button
