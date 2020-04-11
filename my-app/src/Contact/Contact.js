edb1db0a8c9205c25e19d59ac50e8561b2c51ce5
Ehsan Gazar <ehsan.gazar@beteasy.com.au>
2020-04-11 21:47:58 +1000

    fix: 011 - React - useState

--- my-app/src/Contact/Contact.js
+++ my-app/src/Contact/Contact.js
@@ -1,30 +1,52 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Button from '../Button/Button'
 import './Contact'
 
 const Contact = () => {
+    const [subject, setSubject] = useState('');
+    const [email, setEmail] = useState('');
+    const [description, setDescription] = useState('');
+    const [errors,setErrors] = useState({});
 
-    const handleChangeInputText = (event) => {
-      console.log("event", event.target.value);
+    const handleChangeInputSubject = (event) => {
+      setSubject(event.target.value);
     };
 
+    const validateEmail = (email) => {
+      var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+      return re.test(String(email).toLowerCase());
+    }
+
     const handleChangeInputEmail = (event) => {
-      console.log("event", event.target.value);
+        if (validateEmail(event.target.value)){
+            setEmail(event.target.value);
+            setErrors({
+                ...errors,
+                email: null,
+            })
+        }else {
+            setErrors({
+              ...errors,
+              email: 'Email is not valid'
+            });
+        }
     };
 
     const handleChangeTextArea = (event) => {
-      console.log("event", event.target.value);
+      setDescription(event.target.value);
     };
 
     const handleSubmit = () => {
-      console.log('submitted')
+      console.log("subject", subject);
+      console.log("email", email);
+      console.log("description", description);
     };
 
     return (
       <div className="Contact">
         <div className="formControl">
           <input
-            onChange={handleChangeInputText}
+            onChange={handleChangeInputSubject}
             type="text"
             placeholder="subject"
           />
@@ -35,6 +57,7 @@ const Contact = () => {
             type="email"
             placeholder="Email"
           />
+          {errors.email &&<span>{errors.email}</span>}
         </div>
         <div className="formControl">
           <textarea onChange={handleChangeTextArea}>Your request here</textarea>
