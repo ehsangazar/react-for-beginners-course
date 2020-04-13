275133ea424402fc211796973e8d1465a0a63b55
Ehsan Gazar <ehsan.gazar@beteasy.com.au>
2020-04-13 17:32:52 +1000

    fix: 020 - React - useLocation

--- my-app/src/components/Header/Header.js
+++ my-app/src/components/Header/Header.js
@@ -1,12 +1,35 @@
 import React from 'react'
 import './Header.css'
 import NavBar from './../NavBar/Navbar'
+import { useLocation } from 'react-router-dom'
 
-const Header = () => (
-  <div className="Header">
-    <NavBar />
-    <h3>Shop</h3>
-  </div>
-);
+const Header = () => {
+  let { pathname } = useLocation();
+  let title = '';
+
+  switch (pathname) {
+    case '/':
+      title = 'Shop'
+      break;
+    case '/about':
+      title = 'About'
+      break;
+    case '/blog':
+      title = 'Blog'
+      break;
+    case '/contact':
+      title = 'Contact'
+      break;
+    default:
+      break;
+  }
+
+  return (
+    <div className="Header">
+      <NavBar />
+      <h3>{title}</h3>
+    </div>
+  )
+};
 
 export default Header
